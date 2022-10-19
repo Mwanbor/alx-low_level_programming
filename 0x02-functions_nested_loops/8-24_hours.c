e1ceb7afdc35fe5233c513a48a8d7119488adab4
mwanbor <devyne76@gmail.com>
2022-10-19 06:18:26 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,7 @@
  * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void);
+
 {
 	int hour, minute;
 
@@ -14,6 +15,7 @@ void jack_bauer(void);
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
