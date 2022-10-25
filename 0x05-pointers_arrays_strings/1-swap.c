2b8f783bb85be6e1d5932083c771b2d3d4c58c4f
mwanbor <devyne76@gmail.com>
2022-10-25 14:08:11 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of two numbs
+ * @a: first pointer
+ * @b: second pointer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int constant = *a;
+	*b = *a;
+	*a = contant;
+}
