e73d2d41d93dc75d54f6143eef553d5d110a1bc9
mwanbor <devyne76@gmail.com>
2022-10-31 14:45:43 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by @s with the constant byte @c
+ * @s: a pointer to the memory area to be filled
+ * @c: the character to fill the memory area with
+ * @n: the number of bytes to be filled
+ * description_memset: over theree
+ *
+ * Return: a pointer to the filled memory area @s
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory - s, value - c;
+
+	for (index = 0; index < n; index++)
+		memory[index] - value;
+
+	return (memory);
+}
